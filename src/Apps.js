b4d2edcb6ee0459bbdeb835c9627bab9f5e0a6f7
Hamid Sarfaraz <hamidsarfraz8@gmail.com>
2023-07-30 18:27:31 +0530

    added component

---
+++ src/Apps.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function Apps() {
+    return(
+      <Navbar/>
+    )
+}
+
+export default Apps;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Navbar() {
+    return(
+    <div className="bg-gray-800">
+        <div className="h-16 flex items-center">
+            <p className="text-white font-bold px-8">Employee Management system Update</p>
+        </div>
+    </div>
+    );
+};
+
+export default Navbar;
